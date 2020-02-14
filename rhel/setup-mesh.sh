1929aa0edcb5efdbd46d7d198a2a033f449b1d4f
Johannes Schüth <git@jotschi.de>
2020-02-14 20:30:07 +0100

    Add default config files

--- rhel/setup-mesh.sh
+++ rhel/setup-mesh.sh
@@ -16,6 +16,10 @@ MESH_INITIAL_ADMIN_PASSWORD=${MESH_INITIAL_ADMIN_PASSWORD:-admin}
 MESH_BINARY_DOCUMENT_PARSER=${MESH_BINARY_DOCUMENT_PARSER:-false}
 MESH_INITIAL_ADMIN_PASSWORD=${MESH_INITIAL_ADMIN_PASSWORD:-admin}
 
+# Clustering
+MESH_WRITE_QUORUM=${MESH_WRITE_QUORUM:-2}
+MESH_READ_QUORUM=${MESH_READ_QUORUM:-1}
+
 # Memory
 MESH_HEAP_MEM=${MESH_HEAP_MEM:-2048}
 MESH_DIRECT_MEM=${MESH_DIRECT_MEM:-512}
@@ -30,7 +34,6 @@ MESH_TEMP_DIR=${MESH_TEMP_DIR:-/opt/mesh-server/temp}
 MESH_BINARY_DIR=${MESH_BINARY_DIR:-/opt/mesh-server/uploads}
 MESH_GRAPH_EXPORT_DIRECTORY=${MESH_GRAPH_EXPORT_DIRECTORY:-/opt/mesh-server/exports}
 MESH_GRAPH_BACKUP_DIRECTORY=${MESH_GRAPH_BACKUP_DIRECTORY:-/opt/mesh-server/backups}
-
 
 # Monitoring
 MESH_MONITORING_HTTP_PORT=${MESH_MONITORING_HTTP_PORT:-8081}
@@ -52,12 +55,86 @@ if [ ! -e $MESH_COMMON_DIR/mesh-server-$MESH_VERSION.jar ] ; then
   wget https://maven.gentics.com/maven2/com/gentics/mesh/mesh-server/$MESH_VERSION/mesh-server-$MESH_VERSION.jar
 fi
 
-mkdir -p $MESH_DIR
+mkdir -p $MESH_DIR/config
 MESH_GRAPH_DB_DIRECTORY=${MESH_DIR}/graphdb
 MESH_LOCK_PATH=${MESH_DIR}/.meshlock
 ln -s mesh-server-$MESH_VERSION.jar mesh-server.jar
 
-cat <<EOT > ${MESH_DIR}/mesh-server.service
+echo -e "\nCreating log configuration"
+cat << EOT > ${MESH_DIR}/config/logback.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<configuration scan="true" scanPeriod="30 seconds">
+
+	<conversionRule conversionWord="meshName" converterClass="com.gentics.mesh.log.MeshLogNameConverter"/>
+	<statusListener class="ch.qos.logback.core.status.NopStatusListener" />
+
+  <appender name="FILE" class="ch.qos.logback.core.rolling.RollingFileAppender">
+    <file>log/mesh.log</file>
+    <target>System.err</target>
+    <append>true</append>
+    <encoder>
+      <pattern>%d [%meshName] %-5level [%file:%line] - %msg%n</pattern>
+    </encoder>
+    <rollingPolicy class="ch.qos.logback.core.rolling.TimeBasedRollingPolicy">
+      <fileNamePattern>log/mesh-%d{yyyy-MM}.log.gz</fileNamePattern>
+      <maxHistory>12</maxHistory>
+    </rollingPolicy>
+  </appender>
+
+	<appender name="STDERR" class="ch.qos.logback.core.ConsoleAppender">
+		<target>System.err</target>
+		<encoder>
+			<pattern>%d{HH:mm:ss.SSS} [%meshName] %-5level [%thread] [%logger{20}] - %msg%n</pattern>
+		</encoder>
+		<filter class="ch.qos.logback.classic.filter.LevelFilter">
+			<level>ERROR</level>
+			<onMatch>ACCEPT</onMatch>
+			<onMismatch>DENY</onMismatch>
+		</filter>
+	</appender>
+
+	<appender name="STDOUT" class="ch.qos.logback.core.ConsoleAppender">
+		<encoder>
+			<pattern>%d{HH:mm:ss.SSS} [%meshName] %-5level [%thread] [%logger{20}] - %msg%n</pattern>
+		</encoder>
+	</appender>
+
+	<logger name="io.vertx" level="INFO"/>
+	<logger name="com.gentics" level="INFO"/>
+
+	<root level="ERROR">
+    <appender-ref ref="FILE"/>
+		<appender-ref ref="STDOUT"/>
+	</root>
+</configuration>
+EOT
+
+echo -e "\nCreating default cluster configuration"
+cat << EOT > ${MESH_DIR}/config/default-distributed-db-config.json
+{
+  "autoDeploy": true,
+  "readQuorum": ${MESH_READ_QUORUM},
+  "writeQuorum": ${MESH_WRITE_QUORUM},
+  "hotAlignment": true,
+  "executionMode": "synchronous",
+  "failureAvailableNodesLessQuorum": false,
+  "readYourWrites": true,
+  "newNodeStrategy": "static",
+  "servers": {
+    "*": "master"
+  },
+  "clusters": {
+    "internal": {
+    },
+    "*": {
+      "servers": ["<NEW_NODE>"]
+    }
+  }
+}
+EOT
+
+echo -e "\nCreating service configuration"
+cat << EOT > ${MESH_DIR}/mesh-server.service
 [Unit]
 MESH_Description=$MESH_SERVICE_DESC
 Wants=basic.target
